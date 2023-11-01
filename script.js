db00a7c39da0b62d1927d44216aafcf8a8a99593
daratehexplorer <darinka.lipovac@gmail.com>
2023-11-01 16:47:43 +0000

    Enable button clicks to change calculator display

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+//Display Changes
+let display = document.querySelector('.display')
+
+let numBtns = document.querySelectorAll('.numBtn')
+numBtns.forEach((numBtn) => {
+    let numSelect = numBtn.textContent;
+    numBtn.addEventListener('click', () => {
+        if (display.textContent == "|") {
+            display.textContent = '';
+            display.textContent += numSelect;
+        } else {
+            display.textContent += numSelect;
+        }
+    });
+});
+
+let funcBtns = document.querySelectorAll('.funcBtn')
+funcBtns.forEach((funcBtn) => {
+    let funcSelect = funcBtn.textContent;
+    funcBtn.addEventListener('click', () => {
+        if (display.textContent == "|") {
+            display.textContent = '';
+            display.textContent += funcSelect;
+        } else {
+            display.textContent += funcSelect;
+        }
+    });
+});
+
+let clrBtn = document.querySelector('#clrBtn')
+clrBtn.addEventListener('click', () => {
+    display.textContent = '|';
+})
+
+let equals = document.querySelector('#equBtn')
+equBtn.addEventListener('click', () => {
+    let result = operate();
+    display.textContent = result;
+});
+
 // Math functions
 const add = function(a , b) {
     return a + b;
@@ -15,18 +55,19 @@ const divide = function (a , b) {
     return a / b;
 };
 
-let num1
-let num2
+let firstNum
+let secondNum
 let operator
 
-operate = function(num1, num2, operator) {
+operate = function(firstNum, secondNum, operator) {
     if (operator == "+") {
-       return add(num1, num2);
+       return add(firstNum, secondNum);
     } else if (operator == "-") {
-        return subtract(num1, num2);
+        return subtract(firstNum, secondNum);
     } else if (operator == "*") {
-        return multiply(num1, num2);
+        return multiply(firstNum, secondNum);
     } else {
-        return divide(num1, num2);
+        return divide(firstNum, secondNum);
     };
 }
+
